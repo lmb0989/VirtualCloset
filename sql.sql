750d237f71c2a524cd214ee81bf3b04ba04a9c1f
lmb0989 <1150274785@qq.com>
2015-03-12 17:40:12 +0800

    add sql.sql

---
+++ sql.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.13 : Database - virtualcloset
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`virtualcloset` /*!40100 DEFAULT CHARACTER SET gbk */;
+
+USE `virtualcloset`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `sex` varchar(2) DEFAULT NULL,
+  `age` int(4) DEFAULT NULL,
+  `job` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+
+/*Data for the table `user` */
+
+insert  into `user`(`username`,`password`,`email`,`phone`,`sex`,`age`,`job`) values ('admin','12345','1150274785@qq.com','13641848002',NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
